4dc62025167883c3c4eddafafe52166b8f678146
亀田茂 <kamedashigeru@BITA01085.local>
2016-04-20 18:44:20 +0900

    memo06 可読性向上 変数削減

--- memo06/app-m06.js
+++ memo06/app-m06.js
@@ -17,17 +17,17 @@ var Memo = (function () {
 
     //メモの初期化処理
     Memo.prototype.init = function () {
-        var _that = this,
+        var that = this,
             bigData = localStorage.bigData;
-        _that.setEditor();
+        that.setEditor();
         if (bigData === undefined || bigData === null) {
             var initData = {},
-                _idx = 'memo1';
-            initData[_idx] = _that.firstMemo();
+                idx = 'memo1';
+            initData[idx] = _that.firstMemo();
             pushData(initData);
         }
-        _that.view();
-        _that.viewFav();
+        that.view();
+        that.viewFav();
     };
 
     //iframeをメモ化させる
@@ -37,108 +37,110 @@ var Memo = (function () {
 
     //メモ帳をまっさらにする時の処理
     Memo.prototype.emptyTexts = function () {
-        elSet.$title.val('');
-        elSet.$editor.contents().find('body').html('');
-        elSet.$fav.removeClass('fav');
+        $('#title').val('');
+        $('#editor').contents().find('body').html('');
+        $('#btn-fav').removeClass('fav');
     };
 
     //メモの新規登録機能時の処理
     Memo.prototype.save = function () {
-        var _that = this;
+        var that = this;
         saveNewData(setTitle(), setText());
-        _that.emptyTexts();
-        _that.view();
+        that.emptyTexts();
+        that.view();
     };
 
     //メモの途中セーブ機能の処理
     Memo.prototype.update = function () {
-        var _that = this,
-            _text = setText(),
-            _title = elSet.$title.val(),
-            _idx = elSet.$editor.attr('class'),
-            _fav = elSet.$fav.attr('class');
-        if (_text !== '' || _text !== undefined) {
-            updateData(_title, _text, _idx, _fav);
+        var that = this,
+            text = setText(),
+            title = $('#title').val(),
+            idx = $('#editor').attr('class'),
+            fav = $('#btn-fav').attr('class');
+        if (text !== '' || text !== undefined) {
+            updateData(title, text, idx, fav);
         } else {
             return;
         }
-        _that.view();
-        _that.viewFav();
+        that.view();
+        that.viewFav();
     };
 
     //お気に入りボタンを押した時の処理
     Memo.prototype.fav = function () {
-        var _that = this,
+        var that = this,
             data = pullData(),
-            _idx = elSet.$editor.attr('class'),
-            _title = data[_idx].title,
-            _text = data[_idx].text,
-            _fav = elSet.$fav.attr('class');
-        if (_text !== '' || _text !== undefined) {
-            updateData(_title, _text, _idx, _fav);
+            idx = $('#editor').attr('class'),
+            title = data[idx].title,
+            text = data[idx].text,
+            fav = $('#btn-fav').attr('class');
+        if (text !== '' || text !== undefined) {
+            updateData(title, text, idx, fav);
         } else {
             return;
         }
-        _that.viewFav();
+        that.viewFav();
     };
 
     //メモ削除時の処理
     Memo.prototype.trash = function () {
-        var _that = this,
-            _idx = elSet.$editor.attr('class');
-        deleteData(_idx);
-        _that.emptyTexts();
-        _that.view();
-        _that.viewFav();
+        var that = this,
+            idx = $('#editor').attr('class');
+        deleteData(idx);
+        that.emptyTexts();
+        that.view();
+        that.viewFav();
     };
 
     //リストからメモの情報を引っ張ってくる機能の処理
     Memo.prototype.getData = function (n) {
-        var _idx = n,
-            _data = retrieveData(_idx),
-            $editor = elSet.$editor.contents().find('body');
-        $editor.html(_data.text);
-        if (_data.fav === 'fav') {
-            elSet.$fav.attr('class', _data.fav);
-        } else if (!_data.fav) {
-            elSet.$fav.removeClass('fav');
+        var idx = n,
+            data = retrieveData(_idx),
+            editor = $('#editor').contents().find('body');
+        editor.html(_data.text);
+        if (data.fav === 'fav') {
+            $('#btn-fav').attr('class', data.fav);
+        } else if (!data.fav) {
+            $('#btn-fav').removeClass('fav');
         }
-        elSet.$title.val(_data.title);
-        elSet.$editor.attr('class', _idx);
+        $('#title').val(data.title);
+        $('#editor').attr('class', idx);
     };
 
     //View機能の処理
     Memo.prototype.view = function () {
         var data = pullData(),
-            _text = '';
-        elSet.$list.children().remove();
+            text = '';
+        $('#list').children().remove();
         for (var memo in data) {
-            var date = data[memo].date;
+            var title = data[memo].title,
+                date = data[memo].date;
             if (data[memo].idx === null) {
                 continue;
             } else {
-                _text += '<li class=' + memo + '>' + data[memo].title + '<span class="date">' + date + '</span></li>';
+                text += '<li class=' + memo + '>' + title + '<span class="date">' + date + '</span></li>';
             }
         }
-        elSet.$list.append(_text);
+        $('#list').append(text);
     };
 
     //View機能のお気に入りのみの表示処理
     Memo.prototype.viewFav = function () {
         var data = pullData(),
-            _text = '';
-        elSet.$favList.children().remove();
+            text = '';
+        $('#list-fav').children().remove();
         for (var memo in data) {
             if (data[memo].idx === null) {
                 continue;
             } else if (data[memo].fav === 'fav') {
-                var date = data[memo].date;
-                _text += '<li class=' + memo + '>' + data[memo].title + '<span class="date">' + date + '</span></li>';
+                var title = data[memo].title,
+                    date = data[memo].date;
+                text += '<li class=' + memo + '>' + title + '<span class="date">' + date + '</span></li>';
             } else {
                 continue;
             }
         }
-        elSet.$favList.append(_text);
+        $('#list-fav').append(text);
     };
 
     return Memo;
--- memo06/controller-m06.js
+++ memo06/controller-m06.js
@@ -1,35 +1,35 @@
 'use strict';
 
 //操作：セーブ
-elSet.$save.click(function () {
+$('#btn-save').click(function () {
     memo.save();
 });
 
 //操作：アップデート
-elSet.$update.on('click', function () {
+$('#btn-update').on('click', function () {
     memo.update();
 });
 
 //操作：お気に入り登録
-elSet.$fav.click(function () {
-    var $fav = elSet.$fav.attr('class');
-    if (!$fav) {
-        elSet.$fav.attr('class', 'fav');
+$('#btn-fav').click(function () {
+    var fav = $('#btn-fav').attr('class');
+    if (!fav) {
+        $('#btn-fav').attr('class', 'fav');
     } else {
-        elSet.$fav.removeClass('fav');
+        $('#btn-fav').removeClass('fav');
     }
     memo.fav();
 });
 
 //操作：削除
-elSet.$delete.click(function () {
+$('#btn-delete').click(function () {
     memo.trash();
 });
 
 //操作：お気に入りのみ表示、全体表示のトグル
-elSet.$favBtn.click(function () {
-    elSet.$list.toggle();
-    elSet.$favList.toggle();
+$('#btn-favlist').click(function () {
+    $('#list').toggle();
+    $('#list-fav').toggle();
 });
 
 //操作：一覧から編集したいメモを選ぶ ※documentを指定しないと動的に生成されたDOMを選択できない
